19c09e776a2d91d951fbb4ca225f17ae2686abce
EduhDev <eduardocarvalho970@gmail.com>
2019-06-11 22:13:53 -0300

    route to images and cors enabled

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const mongooseKey = require('../credentials/universal_key.json').key;
+const path = require('path');
+const cors = require('cors');
+
 const app = express();
 
 mongoose.connect(`mongodb+srv://eduardo:${mongooseKey}@cluster0-b9grj.mongodb.net/test?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
 });
+
+app.use(cors());
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
 app.use(require('./routes'));
 
