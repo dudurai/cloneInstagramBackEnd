e1885c69e47f87367babc918c35582ecb3558dd4
EduhDev <eduardocarvalho970@gmail.com>
2019-10-02 22:08:44 -0300

    Real time function

--- src/index.js
+++ src/index.js
@@ -1,20 +1,33 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const mongooseKey = require('../credentials/universal_key.json').key;
-const path = require('path');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const mongooseKey = require("../credentials/universal_key.json").key;
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
-mongoose.connect(`mongodb+srv://eduardo:${mongooseKey}@cluster0-b9grj.mongodb.net/test?retryWrites=true&w=majority`, {
-  useNewUrlParser: true,
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+mongoose.connect(
+  `mongodb+srv://eduardo:${mongooseKey}@cluster0-b9grj.mongodb.net/test?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true
+  }
+);
+
+app.use((req, res, next) => {
+  req.io = io;
+
+  next();
 });
 
 app.use(cors());
 
-app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "uploads", "resized"))
+);
 
-app.use(require('./routes'));
+app.use(require("./routes"));
 
-app.listen(3333);
-
+server.listen(3333);
